2e7be97d7beb2d7caf1c0f45076c28acb026a595
Chris_S <chris.s@apluskitchen.com>
2024-08-13 09:45:56 -0700

    setup routes

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
+/******************************************************** Package Imports *******************************************************/
+
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+/******************************************************** Page Routes *******************************************************/
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
