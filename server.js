1bf71887566719b3887ea1d24322307077a2736d
Chris_S <chris.s@apluskitchen.com>
2024-08-21 09:29:03 -0700

    added routes folder and home.js

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
-  
-  app.get('/notes', (req, res) => {
+});
+
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
-  });
-  
-  app.listen(PORT, () => {
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  });
+});
