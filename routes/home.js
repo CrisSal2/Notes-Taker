90b541399e42f925d9d804b5e0f7886723c77278
CrisSal2 <salgado.chris.m@gmail.com>
2024-08-22 19:54:47 -0700

    added comments, images, adn updated README file

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,12 @@ const path = require('path');
 const router = express.Router();
 
 
-
-
+// Gets notes.html file.
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// Gets index.html file.
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
